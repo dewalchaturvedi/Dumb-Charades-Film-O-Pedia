66b95a6541dbebafa66ab5f9fd4f24fb83902232
dewal <dewalchaturvediofficial@gmail.com>
2020-08-28 11:06:37 +0530

    Dumb Charades

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var app=express();
+var request= require("request");
+app.set("view engine" , "ejs");
+app.use(express.static("public"));
+app.get("/",function(req,res){
+	res.render("home");
+});
+
+app.get("/results",function(req,res){
+	var query=req.query.search;
+	var url="http://www.omdbapi.com/?i=tt3896198&apikey=11702825&s="+query;
+	request( url, function(error,response,body){
+		if(error)
+		{
+			res.send("Not Found!");
+		}
+		if(!error && response.statusCode==200){
+			var data=JSON.parse(body);
+			res.render("results" , {data : data});
+		}
+	});
+});
+
+
+app.listen(3000,function(){
+	console.log("Server started on port 3000");
+});
